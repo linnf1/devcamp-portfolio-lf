a639978a8b03a5bd296dc3530b058276eac800f2
lynnf1 <linnf@live.com>
2017-12-01 01:07:13 +0000

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,64 @@
 module ApplicationHelper
+  
+  def login_helper style = ''
+   if current_user.is_a?(GuestUser)
+     (link_to "Register", new_user_registration_path, class: style) + " ".html_safe + 
+     (link_to "Login", new_user_session_path, class: style)
+   else
+     link_to "Logout", destroy_user_session_path, method: :delete, class: style
+   end
+  end
+  
+  def source_helper(layout_name)
+   if session[:source]
+   greeting = "Thanks for coming from  #{session[:source]} and you are on the #{layout_name}"
+   content_tag(:p, greeting, class: "sourc-greeting")
+   end 
+  end
+  
+  def copyright_gen
+     LynnViewTool::Renderer.copyright 'Linsey Falkner', 'All rights reserved'
+  end
+  
+  def nav_items
+  [
+    {
+      url: root_path,
+      title: 'Home'
+    },
+    {
+      url: about_me_path,
+      title: 'About Me'
+    },
+    {
+      url: contact_path,
+      title: 'Contact'
+    },
+    {
+      url: blogs_path,
+      title: 'Blog'
+    },
+    {
+      url: portfolios_path,
+      title: 'Portfolio'
+    },
+    ]
+  end
+  
+  def nav_helper style, tag_type
+    nav_links = ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href ='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>
+"
+    end  
+    
 
-def login_helper style = ''
-       if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) + " ".html_safe + 
-      (link_to "Login", new_user_session_path, class: style)
-       else
-       link_to "Logout", destroy_user_session_path, method: :delete, class: style
-       end
+    nav_links.html_safe
+  end
+   
+  def active? path
+    "active" if current_page? path
+  end
+  
 end
 
-def source_helper(layout_name)
-       if session[:source]
-       greeting = "Thanks for coming from  #{session[:source]} and you are on the #{layout_name}"
-      content_tag(:p, greeting, class: "sourc-greeting")
-       end 
-end
-
-       def copyright_gen
-       LynnViewTool::Renderer.copyright 'Linsey Falkner', 'All rights reserved'
-       end
-
-end
